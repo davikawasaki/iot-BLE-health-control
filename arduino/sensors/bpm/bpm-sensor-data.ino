d280a4c4f35771bc1ad14efecb7d4abadc3bbaa9
vinicius0809 <vinicius0809@gmail.com>
2016-10-12 17:53:20 -0300

    redução do código de BPM
    Foram retiradas algumas funções que não serão usadas no projeto. Entretanto, algumas não podem ser removidas sem teste prévio, como funções relacionadas a interrupções.

--- arduino/sensors/bpm/bpm-sensor-data.ino
+++ arduino/sensors/bpm/bpm-sensor-data.ino
@@ -21,7 +21,7 @@ volatile boolean Pulse = false;     // "True" when User's live heartbeat is dete
 volatile boolean QS = false;        // becomes true when Arduoino finds a beat.
 
 // Regards Serial OutPut  -- Set This Up to your needs
-static boolean serialVisual = true;   // Set to 'false' by Default.  Re-set to 'true' to see Arduino Serial Monitor ASCII Visual Pulse
+static boolean serialVisual = false;   // Set to 'false' by Default.  Re-set to 'true' to see Arduino Serial Monitor ASCII Visual Pulse
 
 
 
@@ -65,10 +65,7 @@ void interruptSetup() {
 
 //  Where the Magic Happens
 void loop() {
-
-
   // serialOutput() ;
-
   if (QS == true) {
     // A Heartbeat Was Found
     // BPM and IBI have been Determined
@@ -95,14 +92,6 @@ void ledFadeToBeat() {
 
 
 //----------------------------------------------------------------------------------------------------------//
-void serialOutput() {  // Decide How To Output Serial.
-  if (serialVisual == true) {
-    arduinoSerialMonitorVisual('-', Signal);   // goes to function that makes Serial Monitor Visualizer
-  } else {
-    sendDataToSerial('S', Signal);     // goes to sendDataToSerial function
-  }
-}
-
 
 //  Decides How To OutPut BPM and IBI Data
 void serialOutputWhenBeatHappens() {
@@ -111,15 +100,15 @@ void serialOutputWhenBeatHappens() {
     // Serial.print("BPM: ");
     Serial.print(BPM);
     Serial.print("\n ");
+    } 
+  else {
+    sendDataToSerial('B', BPM);  // send heart rate with a 'B' prefix
+    sendDataToSerial('Q', IBI);  // send time between beats with a 'Q' prefix
     comandoGet = ">GET " + endServidor + BPM + " HTTP/1.1\r\nHost: " + endHost;
     tamanhoComando = "AT+CIPSEND=" + (comandoGet.length() - 5 - 4); // quantidade de caracteres do comando GET
     sendData(tamanhoComando, 2000, DEBUG);
 
     sendData(comandoGet, 2000, DEBUG);
-
-  } else {
-    sendDataToSerial('B', BPM);  // send heart rate with a 'B' prefix
-    sendDataToSerial('Q', IBI);  // send time between beats with a 'Q' prefix
   }
 }
 
@@ -133,16 +122,6 @@ void sendDataToSerial(char symbol, int data ) {
 }
 
 
-//  Code to Make the Serial Monitor Visualizer Work
-void arduinoSerialMonitorVisual(char symbol, int data ) {
-  const int sensorMin = 0;      // sensor minimum, discovered through experiment
-  const int sensorMax = 1024;    // sensor maximum, discovered through experiment
-
-  int sensorReading = data;
-  // map the sensor range to a range of 12 options:
-
-
-}
 
 //--------------------------------------------------------------------------------------------------
 
@@ -186,69 +165,5 @@ ISR(TIMER2_COMPA_vect) {                        // triggered when Timer2 counts 
       Pulse = true;                               // set the Pulse flag when we think there is a pulse
       digitalWrite(blinkPin, HIGH);               // turn on pin 13 LED
       IBI = sampleCounter - lastBeatTime;         // measure time between beats in mS
-#include <SoftwareSerial.h>
-SoftwareSerial esp8266(2, 3);
-
-#define DEBUG true
-
-//  Variáveis
-int pulsePin = 0;                 // Pulse Sensor purple wire connected to analog pin 0
-int blinkPin = 13;                // pin to blink led at each beat
-int fadePin = 5;                  // pin to do fancy classy fading blink at each beat
-int fadeRate = 0;                 // used to fade LED on with PWM on fadePin
-String comandoGet, tamanhoComando;                       // variável utilizada para o comando GET
-String endServidor = "/healthcontrol/sendData.php?bpm=";
-String endHost = "luiseduardoluz.com\r\n\r\n";
-
-
-// Volatile Variables, used in the interrupt service routine!
-volatile int BPM;                   // int that holds raw Analog in 0. updated every 2mS
-volatile int Signal;                // holds the incoming raw data
-volatile int IBI = 600;             // int that holds the time interval between beats! Must be seeded!
-volatile boolean Pulse = false;     // "True" when User's live heartbeat is detected. "False" when not a "live beat".
-volatile boolean QS = false;        // becomes true when Arduoino finds a beat.
-
-// Regards Serial OutPut  -- Set This Up to your needs
-static boolean serialVisual = true;   // Set to 'false' by Default.  Re-set to 'true' to see Arduino Serial Monitor ASCII Visual Pulse
-
-
-
-void setup() {
-  esp8266.begin(115200);
-  // Reseta o módulo
-  sendData("AT+RST\r\n", 2000, DEBUG);
-  delay(1000);
-  Serial.println("\n\nVersao de firmware");
-  delay(3000);
-  sendData("AT+GMR\r\n", 2000, DEBUG); // rst
-  // Configure na linha abaixo a velocidade desejada para a
-  // comunicacao do modulo ESP8266 (9600, 19200, 38400, etc)
-  sendData("AT+CIOBAUD=115200\r\n", 2000, DEBUG);
-  Serial.println("\n\nConectando na rede Wi-Fi:");
-  delay(3000);
-  sendData("AT+CWJAP=\"123\",\"abc12345\"\r\n", 2000, DEBUG);
-  sendData("AT+CWMODE=1\r\n", 1000, DEBUG);
-  // Mostra o endereco IP
-  Serial.println("\n\nEndereco IP:");
-  delay(3000);
-  sendData("AT+CIFSR\r\n", 1000, DEBUG);
-  Serial.println("\n\n** Final **");
-  pinMode(blinkPin, OUTPUT);        // pin that will blink to your heartbeat!
-  pinMode(fadePin, OUTPUT);         // pin that will fade to your heartbeat!
-  Serial.begin(9600);             // we agree to talk fast!
-  Serial.print("BPM: ");
-  interruptSetup();                 // sets up to read Pulse Sensor signal every 2mS
-  // IF YOU ARE POWERING The Pulse Sensor AT VOLTAGE LESS THAN THE BOARD VOLTAGE,
-  // UN-COMMENT THE NEXT LINE AND APPLY THAT VOLTAGE TO THE A-REF PIN
-  //   analogReference(EXTERNAL);
-}
-void interruptSetup() {
-  // Initializes Timer2 to throw an interrupt every 2mS.
-  TCCR2A = 0x02;     // DISABLE PWM ON DIGITAL PINS 3 AND 11, AND GO INTO CTC MODE
-  TCCR2B = 0x06;     // DON'T FORCE COMPARE, 256 PRESCALER
-  OCR2A = 0X7C;      // SET THE TOP OF THE COUNT TO 124 FOR 500Hz SAMPLE RATE
-  TIMSK2 = 0x02;     // ENABLE INTERRUPT ON MATCH BETWEEN TIMER2 AND OCR2A
-  sei();             // MAKE SURE GLOBAL INTERRUPTS ARE ENABLED
-}
-
-//  Where the Magic Happens
+    }
+  }
