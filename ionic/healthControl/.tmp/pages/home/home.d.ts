dee64ad088dbea901c427f6d1f50bbfbcb3b9223
davikawasaki <davishinjik@gmail.com>
2016-11-24 01:06:01 -0200

    Excluding .tmp build files

--- ionic/healthControl/.tmp/pages/home/home.d.ts
+++
@@ -1,7 +0,0 @@
-import { NavController } from 'ionic-angular';
-export declare class HomePage {
-    navCtrl: NavController;
-    bpm: number;
-    constructor(navCtrl: NavController);
-    goToOtherPage(): void;
-}
--- ionic/healthControl/.tmp/pages/login/login.d.ts
+++
@@ -1,6 +0,0 @@
-import { NavController } from 'ionic-angular';
-export declare class Login {
-    navCtrl: NavController;
-    constructor(navCtrl: NavController);
-    ionViewDidLoad(): void;
-}
